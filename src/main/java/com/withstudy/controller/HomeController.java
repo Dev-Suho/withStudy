e7894428c8f80d31afe258dee7d8d4c0d36a486e
lsh17831048 <ars8129@naver.com>
2022-11-14 06:16:18 +0900

    메인 페이지, 스터디 모임, 커뮤니티, 로그인, 회원가입 페이지 뷰 구현

--- src/main/java/com/withstudy/controller/HomeController.java
+++ src/main/java/com/withstudy/controller/HomeController.java
@@ -1,18 +1,35 @@
 package com.withstudy.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String viewHome() {
+        return "withStudy";
     }
 
-    @GetMapping("/home")
-    public String viewHome() {
-        return "home";
+    @GetMapping("/studyList")
+    public String viewPost() {
+        return "studyListPage";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginPage";
+    }
+
+    @GetMapping("/community")
+    public String communityView() {
+        return "communityPage";
+    }
+
+    @GetMapping("/signUp")
+    public String singUpView() {
+        return "signUpPage";
     }
 }
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,16 +1,24 @@
-const signInBtn = document.getElementById("signIn");
-const signUpBtn = document.getElementById("signUp");
-const fistForm = document.getElementById("form1");
-const secondForm = document.getElementById("form2");
-const container = document.querySelector(".container");
-
-signInBtn.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+function resetClass(element, classname){
+    element.classList.remove(classname);
+}
+document.getElementsByClassName("show-signup")[0].addEventListener("click",function(){
+    let form = document.getElementsByClassName("form")[0];
+    resetClass(form, "signin");
+    resetClass(form, "reset");
+    form.classList.add("signup");
+    document.getElementById("submit-btn").innerText = "Sign Up";
 });
-
-signUpBtn.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
+document.getElementsByClassName("show-signin")[0].addEventListener("click",function(){
+    let form = document.getElementsByClassName("form")[0];
+    resetClass(form, "signup");
+    resetClass(form, "reset");
+    form.classList.add("signin");
+    document.getElementById("submit-btn").innerText = "Sign In";
 });
-
-fistForm.addEventListener("submit", (e) => e.preventDefault());
-secondForm.addEventListener("submit", (e) => e.preventDefault());
+document.getElementsByClassName("show-reset")[0].addEventListener("click",function(){
+    let form = document.getElementsByClassName("form")[0];
+    resetClass(form, "signup");
+    resetClass(form, "signin");
+    form.classList.add("reset");
+    document.getElementById("submit-btn").innerText = "Reset password";
+});
