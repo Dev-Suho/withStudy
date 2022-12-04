fba88c73a11e5bf82df6c6d24c75cd11c0c3eed7
lsh17831048 <ars8129@naver.com>
2022-12-05 06:32:39 +0900

    thymeleaf를 사용하여 스터디 리스트 출력

--- src/main/java/com/withstudy/controller/HomeController.java
+++ src/main/java/com/withstudy/controller/HomeController.java
@@ -1,16 +1,34 @@
 package com.withstudy.controller;
 
 
+import com.withstudy.domain.StudyBoardDTO;
+import com.withstudy.service.StudyBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final StudyBoardService studyBoardService;
+
+    @Autowired
+    public HomeController(StudyBoardService studyBoardService) {
+        this.studyBoardService = studyBoardService;
+    }
+
     @GetMapping(value = {"/withStudy", "/"})
-    public String viewHome() {
-        return "withStudy";
+    public ModelAndView viewHome() throws Exception {
+        List<StudyBoardDTO> studyBoardDTOS = null;
+        studyBoardDTOS = studyBoardService.studyBoardList();
+
+        ModelAndView mav = new ModelAndView("withStudy");
+        mav.addObject("boardList", studyBoardDTOS);
+
+        return mav;
     }
 
     @GetMapping("/login")
